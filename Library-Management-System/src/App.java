6043bddde405c4f2456795fff2aec0ff9e2d30a1
baya-bk <118342926+baya-bk@users.noreply.github.com>
2023-01-19 12:30:30 -0800

    Java project creation.

---
+++ Library-Management-System/src/App.java
@@ -0,0 +1,12 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+ 
+public class App extends Application {
+ 
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello World!");
+        primaryStage.show();
+    }
+  public static void main(String[] args) throws Exception {
+        launch(args);    }
+}
